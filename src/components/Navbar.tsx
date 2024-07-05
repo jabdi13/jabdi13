e39185a917844b20057e259f426fb2d8c1d0adff
Abdiel Ortega <nathanabdiel1@gmail.com>
2024-07-05 05:03:59 -0600

    Hotfix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,6 @@ export const Navbar = () => {
       <img src={logo} width={300} alt="Rick and Morty logo" />
       <ul>
         <li>Characters</li>
-        <li>Locations</li>
-        <li>Episodes</li>
       </ul>
     </nav>
   )
